a6ae9911cdd185140534dab3cc1839f8e7dcd139
arms22 <arms22@gmail.com>
2018-08-01 12:56:01 +0900

    up-to-date

--- inago_backtest.py
+++ inago_backtest.py
@@ -8,12 +8,21 @@ from functools import lru_cache
 
 def inago_backtest(ohlcv, buyth, sellth, eventh):
 
-    # エントリー／イグジット
-    cond = (ohlcv.buy_volume > buyth) & (ohlcv.sell_volume > sellth) & ((ohlcv.buy_volume - ohlcv.sell_volume).abs() > eventh)
-    buy_entry = (ohlcv.buy_volume > ohlcv.sell_volume) & cond & (change(ohlcv.close,3)<2000)
-    sell_entry = (ohlcv.buy_volume < ohlcv.sell_volume) & cond & (change(ohlcv.close,3)>-2000)
-    buy_exit = sell_entry
-    sell_exit = buy_entry
+    # 売り買い優劣でエントリー
+    if 0:
+        buy_entry = (ohlcv.buy_volume > ohlcv.sell_volume) & ((ohlcv.buy_volume - ohlcv.sell_volume).abs() > eventh)
+        sell_entry = (ohlcv.buy_volume < ohlcv.sell_volume) & ((ohlcv.buy_volume - ohlcv.sell_volume).abs() > eventh)
+        buy_exit = sell_entry
+        sell_exit = buy_entry
+
+    # 売り買いサイズがN期間ATRを超えたらエントリー
+    if 1:
+        buy_atr = atr(ohlcv.buy_volume, ohlcv.buy_volume, ohlcv.buy_volume, buyth).shift(1)
+        sell_atr = atr(ohlcv.buy_volume, ohlcv.buy_volume, ohlcv.buy_volume, sellth).shift(1)
+        buy_entry = change(ohlcv.buy_volume) > buy_atr
+        sell_entry = change(ohlcv.sell_volume) > sell_atr
+        buy_exit = sell_entry
+        sell_exit = buy_entry
 
     # entry_exit = pd.DataFrame({'close':ohlcv.close,
     #     'buy_entry':buy_entry, 'buy_exit':buy_exit, 'sell_entry':sell_entry, 'sell_exit':sell_exit})
@@ -25,19 +34,19 @@ def inago_backtest(ohlcv, buyth, sellth, eventh):
 if __name__ == '__main__':
 
     # テストデータ読み込み
-    ohlcv = pd.read_csv('csv/bffx_20180705_10s.csv', index_col='timestamp', parse_dates=True)
+    ohlcv = pd.read_csv('csv/bf28sep2018_1s.csv', index_col='timestamp', parse_dates=True, dtype = {'open':'float', 'close':'float', 'high':'float', 'low':'float'})
 
     default_parameters = {
         'ohlcv':ohlcv,
-        'buyth':20,
-        'sellth':20,
-        'eventh':40,
+        'buyth':15,
+        'sellth':5,
+        'eventh':1,
     }
 
     hyperopt_parameters = {
-        'buyth': hp.quniform('buyth', 5, 100, 1),
-        'sellth': hp.quniform('sellth', 5, 100, 1),
-        'eventh': hp.quniform('eventh', 5, 100, 1),
+        'buyth': hp.quniform('buyth', 1, 100, 1),
+        'sellth': hp.quniform('sellth', 1, 100, 1),
+        'eventh': hp.quniform('eventh', 1, 100, 1),
     }
 
     def maximize(r):
--- market_make_backtest.py
+++ market_make_backtest.py
@@ -6,7 +6,7 @@ from indicator import *
 from functools import lru_cache
 
 
-def market_make_backtest(ohlcv, period, margin):
+def market_make_backtest(ohlcv, period, margin, multi):
 
     buy_entry = None
     sell_entry = None
@@ -60,17 +60,42 @@ def market_make_backtest(ohlcv, period, margin):
     # buy_exit = sell_entry
     # sell_exit = buy_entry
 
-    # 出来高でエントリー
-    no = pd.Series(range(len(ohlcv)), index=ohlcv.index)
-    plus_minus_sum = ohlcv.plus_minus.cumsum()
-    corr_plus_minus_sum = plus_minus_sum.rolling(3).corr(no)
-    buy_entry = crossover(corr_plus_minus_sum, 0.2)
-    sell_entry = crossunder(corr_plus_minus_sum, -0.2)
-    buy_exit = sell_entry
-    sell_exit = buy_entry
+    # 売り買い優劣でエントリー4
+    # plus_minus_sum = ohlcv.plus_minus.rolling(int(period)).sum()
+    # plus_minus_sum = sma(plus_minus_sum, 3)
+    # buy_entry = change(plus_minus_sum) > 0
+    # sell_entry = change(plus_minus_sum) < 0
+    # buy_exit = sell_entry
+    # sell_exit = buy_entry
 
-    # signed_volume_sum = ohlcv.signed_volume.cumsum()
-    # corr_signed_volume_sum = signed_volume_sum.rolling(100).corr(ohlcv['id'])
+    # 売り買い優劣でエントリー5
+    # pm = ohlcv.plus_minus.cumsum()
+    # buy_entry = change(pm) > 0
+    # sell_entry = change(pm) < 0
+    # buy_exit = change(pm) < 0
+    # sell_exit = change(pm) > 0
+
+    # 売り買い優劣でエントリー6
+    # sv = ohlcv.signed_volume.cumsum()
+    # buy_entry = change(sv) > 0
+    # sell_entry = change(sv) < -0
+    # buy_exit = sell_entry
+    # sell_exit = buy_entry
+
+    # 売り買い優劣でエントリー7
+    # sv = ohlcv.signed_volume
+    # buy_entry = sv > 0
+    # sell_entry = sv < 0
+    # buy_exit = sell_entry
+    # sell_exit = buy_entry
+
+    # 売り買い優劣でエントリー8
+    # buy_atr = atr(ohlcv.buy_volume, ohlcv.buy_volume, ohlcv.buy_volume, 20).shift(1)
+    # sell_atr = atr(ohlcv.buy_volume, ohlcv.buy_volume, ohlcv.buy_volume, 5).shift(1)
+    # buy_entry = change(ohlcv.buy_volume) > buy_atr
+    # sell_entry = change(ohlcv.sell_volume) > sell_atr
+    # buy_exit = sell_entry
+    # sell_exit = buy_entry
 
     # 売り買い優劣でエントリー価格を調整2
     # buy_volume = rsi(ohlcv.buy_volume, 10)
@@ -81,14 +106,15 @@ def market_make_backtest(ohlcv, period, margin):
     # limit_sell_exit = limit_sell_exit + (buy_volume > sell_volume) * 50
 
     # ボリュームでエントリー価格を調整
-    # buy_volume = sma(ohlcv.buy_volume, 1)
-    # sell_volume = sma(ohlcv.sell_volume, 1)
-    # restrict_volume = 20
-    # limit_buy_entry = limit_buy_entry - (buy_volume > restrict_volume) * 50
-    # limit_buy_exit = limit_buy_exit - (buy_volume > restrict_volume) * 50
-    # limit_sell_entry = limit_sell_entry + (sell_volume > restrict_volume) * 50
-    # limit_sell_exit = limit_sell_exit + (sell_volume > restrict_volume) * 50
+    # vol_sum = ohlcv.volume.rolling(int(period)).sum()
+    # vol_avg = sma(ohlcv.volume, period)
+    # price_range = ((vol_sum + vol_avg) * 1.5) + 20
+    # limit_buy_entry = ohlcv.close - (price_range / 2)
+    # limit_sell_entry = ohlcv.close + (price_range / 2)
+    # limit_buy_exit = ohlcv.high
+    # limit_sell_exit = ohlcv.low
 
+    # 高値安値でエントリー
     # range_high = highest(ohlcv.high, period)
     # range_low = lowest(ohlcv.low, period)
     # limit_buy_entry = range_low
@@ -96,15 +122,31 @@ def market_make_backtest(ohlcv, period, margin):
     # limit_sell_entry = range_high
     # limit_sell_exit = range_low
 
+    # ATRでエントリー
+    # vatr = atr(ohlcv.close, ohlcv.high, ohlcv.low, period) * multi
+    # limit_buy_entry = ohlcv.close - vatr
+    # limit_sell_entry = ohlcv.close + vatr
+    # limit_buy_exit = limit_sell_entry
+    # limit_sell_exit = limit_buy_entry
+
+    # limit_buy_entry[(high - low)<100] = 0
+    # limit_sell_entry[(high - low)<100] = 0
+
+    # 安く買って高く売る
+    limit_buy_entry = ohlcv.low - margin
+    limit_buy_exit = ohlcv.high + margin
+    limit_sell_entry = ohlcv.high + margin
+    limit_sell_exit = ohlcv.low - margin
+
     # バックテスト実施
-    # entry_exit = pd.DataFrame({'close':ohlcv['close'], 'high':ohlcv['high'], 'low':ohlcv['low'],
+    # entry_exit = pd.DataFrame({'close':ohlcv['close'], 'high':ohlcv['high'], 'low':ohlcv['low'],# 'plus_minus_sum':plus_minus_sum, "corr_plus_minus_sum":corr_plus_minus_sum,
     #     'limit_buy_entry':limit_buy_entry, 'limit_buy_exit':limit_buy_exit, 'limit_sell_entry':limit_sell_entry, 'limit_sell_exit':limit_sell_exit,
     #     'buy_entry':buy_entry, 'buy_exit':buy_exit, 'sell_entry':sell_entry, 'sell_exit':limit_sell_exit,
     #     })
     # entry_exit.to_csv('entry_exit.csv')
 
-    buy_size = sell_size = 0.01
-    max_buy_size = max_sell_size = 0.01
+    # buy_size = sell_size = 0.001
+    # max_buy_size = max_sell_size = 0.001
 
     return Backtest(**locals())
     # return BacktestWithTickData(**locals())
@@ -113,23 +155,24 @@ def market_make_backtest(ohlcv, period, margin):
 if __name__ == '__main__':
 
     # テストデータ読み込み
-    ohlcv = pd.read_csv('csv/bffx_20180618_3s.csv', index_col="timestamp", parse_dates=True)
+    ohlcv = pd.read_csv('csv/bf28sep2018_1s.csv', index_col="timestamp", parse_dates=True)
+    ohlcv = ohlcv['2018-7-29':]
     # ohlcv = ohlcv[:3000]
-    # ticks = pd.read_csv('csv/bffx_2018-6-22.csv', names=['id', 'side', 'price', 'size', 'exec_date'], index_col="exec_date", parse_dates=True)
-    # ticks = ticks[300000:]
+    # ticks = pd.read_csv('csv/bffx_20180628_executions.csv', names=['id', 'side', 'price', 'size', 'exec_date'], index_col="exec_date", parse_dates=True)
+    #ticks = ticks[:100000]
 
     default_parameters = {
         # 'ticks': ticks,
         'ohlcv': ohlcv,
-        'period': 5,
-        'margin': 2000,
+        'period': 200,
+        'margin': 800,
+        'multi': 3,
     }
 
     hyperopt_parameters = {
-        # 'period': hp.quniform('period', 1, 100, 1),
-        # 'margin': hp.quniform('margin', 1, 50, 0.5),
-        # 'period': hp.quniform('period', 10, 1000, 10),
-        # 'margin': hp.uniform('margin', 0.0001, 0.01),
+        'period': hp.quniform('period', 1, 1000, 1),
+        'margin': hp.uniform('margin', 10, 10000),
+        'multi': hp.uniform('multi', 0.1, 5.0),
     }
 
     def maximize(r):
