977ea3359cba86232fbcf2909fe9239693504e1f
arms22 <arms22@gmail.com>
2019-01-24 17:48:09 +0900

    update smm_backtest.py

--- smm_backtest.py
+++ smm_backtest.py
@@ -6,26 +6,44 @@ from indicator import *
 
 def simple_market_make_backtest(ohlcv):
 
-    def yourlogic(O, H, L, C, n, position_size, **others):
+    def smm_logic1(O, H, L, C, n, position_size, **others):
         orders = []
-
         maxsize = 0.05
         buysize = sellsize = 0.025
         spr = C * 0.00225
         buy = C - spr/2
         sell = C + spr/2
-
         if position_size < maxsize:
             orders.append((+1, buy, buysize, 'L'))
         else:
             orders.append((0, 0, 0, 'L'))
-
         if position_size > -maxsize:
             orders.append((-1, sell, sellsize, 'S'))
         else:
             orders.append((0, 0, 0, 'S'))
-
         return orders
+
+    def smm_logic2(O, H, L, C, n, position_size, **others):
+        orders = []
+        pairs = [(0.01, 50), (0.02, 100), (0.04, 200)]
+        maxsize = sum(p[0] for p in pairs)
+        buymax = sellmax = position_size
+        for pair in pairs:
+            suffix = str(pair[1])
+            buymax += pair[0]
+            sellmax -= pair[0]
+            if buymax < maxsize:
+                orders.append((+1, C-pair[1], pair[0], 'L'+suffix))
+            else:
+                orders.append((0, 0, 0, 'L'+suffix))
+            if sellmax > -maxsize:
+                orders.append((-1, C+pair[1], pair[0], 'S'+suffix))
+            else:
+                orders.append((0, 0, 0, 'S'+suffix))
+        return orders
+
+    # yourlogic = smm_logic1
+    yourlogic = smm_logic2
 
     return Backtest(**locals())
 
